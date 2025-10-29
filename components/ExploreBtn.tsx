4e7e922f48f85665d982149cd332b20a4b423190
Rushikesh1 Avachat <avachatrushikesh45@gmail.com>
2025-10-29 11:29:05 +0530

    Implement PostHog

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -1,10 +1,11 @@
-'use client';
 
+'use client'
 import Image from "next/image";
+
 
 const ExploreBtn = () => {
     return (
-        <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={() => console.log('CLICK')}>
+        <button type="button" id="explore-btn" className="mt-7 mx-auto" >
             <a href="#events">
                 Explore Events
                 <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24} />
